e37eccd2cbdbde7d5aed045cced6d9fdbddd41a8
Sucharinio <maciej.kwiatkowski92@gmail.com>
2014-11-27 03:15:03 +0100

    zaj4 zad3

--- tasks/zaj4/zadanie3.py
+++ tasks/zaj4/zadanie3.py
@@ -67,16 +67,32 @@ class Integrator(object):
         :param level: Stopień metody NC
         """
         self.level = level
+        
+    def n_c_integral(self, func, func_range):
+        #h = (func_range[1]-func_range[0])/(self.level-1)
+        params = np.asarray(PARAMS[self.level])
+        integral = np.linspace(func_range[0],func_range[1]),(self.level-1)
+        integral = func(integral)*params
+        #for i in range(self.level):
+        #    integral += params[i]*func(func_range[0]+h*i)
+        return sum(integral)*(func_range[1]-func_range[0])/(self.level-1)
 
     def integrate(self, func, func_range, num_evaluations):
         """
-
         :param callable func: Funkcja którą całkujemy
         :param tuple[int] func_range: Krotka zawierająca lewą i prawą granicę całkowania
         :param in tnum_evaluations:
         :return:
         """
-
+        num_of_steps = np.floor(num_evaluations/self.level)#liczba binów
+        step = (func_range[1]-func_range[0])/num_of_steps#długość binów
+        integral = np.ones((num_of_steps, self.level))#alokacja macierzy
+        integral = integral*np.linspace(func_range[0], func_range[0]+step, self.level)#utworzenie wektorów do metody
+        integral = integral*np.linspace(func_range[0],func_range[1]-step, num_of_steps)[:,np.newaxis]#rozsuniecie binów
+        print(integral)
+        params = np.asarray(self.PARAMS[self.level])
+        integral = np.sum(func(integral)*params)*(func_range[1]-func_range[0])/self.level/num_of_steps#sumowanie i przeskalowanie
+        return integral
 
 
 if __name__ == "__main__":
